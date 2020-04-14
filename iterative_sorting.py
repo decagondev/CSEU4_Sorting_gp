6620dc132bf92b3d60c4bae745cf9d046f671301
Tom <tomtarpeydev@gmail.com>
2020-04-14 11:44:23 +0100

    selection sort solution

--- iterative_sorting.py
+++ iterative_sorting.py
@@ -1,20 +1,58 @@
 # TO-DO: Complete the selection_sort() function below 
+# def selection_sort( arr ):
+#     # loop through n-1 elements
+#     # len(arr) => 5
+#     # out of bounds
+#     # len(arr) - 1
+#     # 4 -> 3 
+#     for i in range(0, len(arr) - 1): // OBOB
+#         cur_index = i
+#         smallest_index = cur_index
+#         # TO-DO: find next smallest element
+#         # (hint, can do in 3 loc) 
+#         # iterate over a range
+#         for j in range(cur_index, len(arr)):
+#             if arr[j] < arr[smallest_index]:
+#                 smallest_index = j
+
+        
+#         # swap
+#         arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+
+#     return arr
+
+# TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
+        # set sorted item index to i
         cur_index = i
+
+        # give smallest a sentinal value
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-        # iterate over a range
-        for j in range(cur_index, len(arr)):
+        # loop over elements at right hand side of current index (current index + 1)
+        # our ranege is the current index to the len of array - 1
+        # loop here
+        for j in range(cur_index + 1, len(arr)):
+            # check if the number current iteration index is smaller than 
+            # the number at the smallest index
+            # if condition here
             if arr[j] < arr[smallest_index]:
+                # if true then set smallest index to iteration index
                 smallest_index = j
-
         
-        # swap
+        # swap number at current index for number at smallest index
         arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+        # arr[smallest_index], arr[cur_index] = 5, 3
 
+
+        # arr[smallest_index] = 5
+        # arr[cur_index] = 3
+
+        # a, b = 5, 3
+
+    # return the array
     return arr
 
 # implement the Insertion Sort function below
