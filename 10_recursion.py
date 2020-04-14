4cc1d4752c83bf372f8d57ba11b04364beca4328
Tom <tomtarpeydev@gmail.com>
2020-04-14 11:16:43 +0100

    quicksort

--- 10_recursion.py
+++ 10_recursion.py
@@ -1,12 +1,25 @@
-n = 100
-while n > 0:
-    print(n)
-    n -= 1
+import sys
+sys.setrecursionlimit(1000)
+n = 40
 
-def rec_while(n): # O(n)
-    # base case
-    if n == 0:
-        return
-    # logic that we want to do
-    print(n)
-    rec_while(n - 1)
+# use space in a linear format
+# while n != 0: # O(n)
+#     print(n)
+#     n -= 1
+
+# def rec_while(n): # O(n)
+#     # base case
+#     if n == 0:
+#         return
+#     # logic that we want to do
+#     print(n)
+#     rec_while(n - 1)
+
+# rec_while(n)
+
+def fib(n):
+    if n < 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+print(fib(n))
